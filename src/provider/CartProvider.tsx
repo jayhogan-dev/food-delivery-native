8f1706f64cceca16897cc3355350c9db1fd001dd
Jay Hogan <jayhogan.dev@gmail.com>
2024-03-14 20:03:04 -0700

    total added to shopping cart

--- src/provider/CartProvider.tsx
+++ src/provider/CartProvider.tsx
@@ -6,12 +6,14 @@ type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
   updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number;
 };
 
 const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
   updateQuantity: () => {},
+  total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -51,8 +53,15 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     setItems(updatedItems);
   };
 
+  const notRoundedTotal = items.reduce(
+    (sum, item) => (sum += item.product.price * item.quantity),
+    0
+  );
+
+  const total = parseFloat(notRoundedTotal.toFixed(2));
+
   return (
-    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );
